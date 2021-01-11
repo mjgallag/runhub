b3cac1c1242bc2c1e6e526b6d155261abf169d0a
Michael Gallagher <mjgallag@gmail.com>
2021-01-11 12:07:10 -0800

    build(infra-gcs): update google provider to 3.52.0 from 3.51.1

--- terraform/development/versions.tf
+++ terraform/development/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.51.1"
+      version = ">= 3.52.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.51.1"
+      version = ">= 3.52.0"
     }
   }
 }
