894ad4250fd43f78463d3458bced12ea60d7300c
Michael Gallagher <mjgallag@gmail.com>
2021-01-11 10:14:19 -0800

    refactor(infra-gcs): rename resources

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -4,26 +4,26 @@ locals {
   isDev           = var.environment == "dev"
 }
 
-data "google_billing_account" "billing_account" {
+data "google_billing_account" "app_env" {
   display_name = var.billing_account
   open         = true
 }
 
-resource "google_project" "project" {
+resource "google_project" "app_env" {
   project_id          = local.project_id_name
   name                = local.project_id_name
-  billing_account     = data.google_billing_account.billing_account.id
+  billing_account     = data.google_billing_account.app_env.id
   auto_create_network = false
 }
 
-resource "google_project_default_service_accounts" "project_default_service_accounts" {
-  project = google_project.project.project_id
+resource "google_project_default_service_accounts" "app_env" {
+  project = google_project.app_env.project_id
   action  = "DELETE"
 }
 
-resource "google_storage_bucket" "terraform_state_storage_bucket" {
-  project                     = google_project.project.project_id
-  name                        = join("-", [google_project.project.project_id, "terraform", "state"])
+resource "google_storage_bucket" "app_env_terraform_state" {
+  project                     = google_project.app_env.project_id
+  name                        = join("-", [google_project.app_env.project_id, "terraform", "state"])
   location                    = local.location
   uniform_bucket_level_access = true
   versioning {
@@ -31,17 +31,17 @@ resource "google_storage_bucket" "terraform_state_storage_bucket" {
   }
 }
 
-resource "google_project_service" "project_service_artifact_registry" {
+resource "google_project_service" "app_env_artifact_registry" {
   count   = local.isDev ? 1 : 0
-  project = google_project.project.project_id
+  project = google_project.app_env.project_id
   service = "artifactregistry.googleapis.com"
 }
 
-resource "google_artifact_registry_repository" "artifact_registry_repository" {
+resource "google_artifact_registry_repository" "app_env_app" {
   count         = local.isDev ? 1 : 0
-  depends_on    = [google_project_service.project_service_artifact_registry]
+  depends_on    = [google_project_service.app_env_artifact_registry[0]]
   provider      = google-beta
-  project       = google_project.project.project_id
+  project       = google_project.app_env.project_id
   repository_id = "app"
   location      = local.location
   format        = "DOCKER"
