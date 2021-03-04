47ed0dff312a855005b75b5db6eccb375de881e0
Michael Gallagher <mjgallag@gmail.com>
2021-03-04 14:23:42 -0800

    fix: prevent pipelines upgrade from temporarily reverting feature flag

--- scripts/cluster-package-installers/tekton/pipelines.sh
+++ scripts/cluster-package-installers/tekton/pipelines.sh
@@ -4,9 +4,8 @@ set -e
 VERSION='0.21.0'
 KUBECTL="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/kubectl.sh")"
 
-"${KUBECTL:?}" apply --filename \
-  "https://storage.googleapis.com/tekton-releases/pipeline/previous/v${VERSION:?}/release.yaml"
-"${KUBECTL:?}" patch configmap/feature-flags \
-  --namespace tekton-pipelines \
-  --type merge \
-  --patch '{"data":{"disable-affinity-assistant":"true"}}'
+
+curl -L \
+  "https://storage.googleapis.com/tekton-releases/pipeline/previous/v${VERSION:?}/release.yaml" \
+  | sed 's/disable-affinity-assistant: "false"/disable-affinity-assistant: "true"/' \
+  | kubectl apply --filename -
