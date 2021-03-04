d4616ea1f767dbdb5a6fd8f7472212dfd4e0b2aa
Michael Gallagher <mjgallag@gmail.com>
2021-03-03 17:52:57 -0800

    style: space it out

--- scripts/cluster-package-installers/knative-serving.sh
+++ scripts/cluster-package-installers/knative-serving.sh
@@ -5,9 +5,12 @@ SERVNG_VERSION='0.20.1'
 NET_ISTIO_VERSION='0.20.0'
 KUBECTL="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/kubectl.sh")"
 
-"${KUBECTL:?}" apply --filename "https://github.com/knative/serving/releases/download/v${SERVNG_VERSION:?}/serving-crds.yaml"
-"${KUBECTL:?}" apply --filename "https://github.com/knative/serving/releases/download/v${SERVNG_VERSION:?}/serving-core.yaml"
-"${KUBECTL:?}" apply --filename "https://github.com/knative/net-istio/releases/download/v${NET_ISTIO_VERSION:?}/release.yaml"
+"${KUBECTL:?}" apply --filename \
+  "https://github.com/knative/serving/releases/download/v${SERVNG_VERSION:?}/serving-crds.yaml"
+"${KUBECTL:?}" apply --filename \
+  "https://github.com/knative/serving/releases/download/v${SERVNG_VERSION:?}/serving-core.yaml"
+"${KUBECTL:?}" apply --filename \
+  "https://github.com/knative/net-istio/releases/download/v${NET_ISTIO_VERSION:?}/release.yaml"
 "${KUBECTL:?}" patch configmap/config-autoscaler \
   --namespace knative-serving \
   --type merge \
