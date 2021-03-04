258083aa5b62be1b29ee21164452cb764ec14d6b
Michael Gallagher <mjgallag@gmail.com>
2021-03-04 14:56:19 -0800

    feat: install runhub app from script

--- install.sh
+++ install.sh
@@ -5,14 +5,23 @@ set -e
 . "$(dirname "${0:?}")/scripts/initialize.sh"
 
 ENVIRONMENT="${1:?}"
+APP="${2:?}"
+HELM="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/helm.sh")"
 
-"${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/istio.sh"
-"${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/cert-manager.sh"
-"${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/knative/serving.sh"
-"${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/knative/net-istio.sh"
+if [ "${ENVIRONMENT:?}" = 'dev' ] || [ "${ENVIRONMENT:?}" = 'prod' ]; then
+  "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/istio.sh"
+  "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/cert-manager.sh"
+  "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/knative/serving.sh"
+  "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/knative/net-istio.sh"
+fi
 
 if [ "${ENVIRONMENT:?}" = 'dev' ]; then
   "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/tekton/pipelines.sh"
   "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/tekton/triggers.sh"
   "${CLUSTER_PACKAGE_INSTALLERS_DIR:?}/tekton/dashboard.sh"
 fi
+
+"${HELM:?}" upgrade --install --atomic "${APP:?}" "${BASE_DIR:?}"/charts/runhub-app \
+  --namespace "${ENVIRONMENT:?}-${APP:?}" --create-namespace \
+  --set "global.environment.${ENVIRONMENT:?}=true" \
+  --values "${BASE_DIR:?}"/values.yaml
