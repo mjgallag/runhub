2b3ea1f9314c7ea48d3e7dc4e5abd3451cce0954
Michael Gallagher <mjgallag@gmail.com>
2021-03-08 14:58:53 -0800

    refactor: remove unused KUBECTL env var reference

--- scripts/cluster-package-installers/app.sh
+++ scripts/cluster-package-installers/app.sh
@@ -4,7 +4,6 @@ set -e
 ENVIRONMENT="${1:?}"
 APP="${2:?}"
 HELM="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/helm.sh")"
-KUBECTL="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/kubectl.sh")"
 
 "${HELM:?}" upgrade --install --atomic "${APP:?}" "${BASE_DIR:?}"/charts/runhub-app \
   --namespace "${ENVIRONMENT:?}-${APP:?}" --create-namespace \
