a3aa89f3b870368b7230b2c820cfb863a131578f
Michael Gallagher <mjgallag@gmail.com>
2021-03-10 08:50:18 -0800

    refactor: split values into dev, prod & shared

--- scripts/cluster-package-installers/app.sh
+++ scripts/cluster-package-installers/app.sh
@@ -8,7 +8,8 @@ HELM="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/helm.sh")"
 set -- upgrade --install --atomic "${APP:?}" "${BASE_DIR:?}"/charts/runhub-app \
   --namespace "${ENV:?}-${APP:?}" --create-namespace \
   --set "global.env.${ENV:?}=true" \
-  --values "${BASE_DIR:?}/values.yaml"
+  --values "${BASE_DIR:?}/values-shared.yaml" \
+  --values "${BASE_DIR:?}/values-${ENV:?}.yaml"
 
 if [ -f "${BASE_DIR:?}/values-dev--prod-k8s-creds.yaml" ]; then
   "${HELM:?}" "$@" --values "${BASE_DIR:?}/values-dev--prod-k8s-creds.yaml"
