df33ac777ed68c0a15aa0c68caf8d572a3dc2b19
Michael Gallagher <mjgallag@gmail.com>
2021-03-11 10:46:41 -0800

    feat: add Google Cloud SDK local package installer

---
+++ scripts/local-package-installers/gcloud.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+VERSION='331.0.0'
+URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${VERSION:?}-darwin-x86_64.tar.gz"
+SHA='32c7b961f4c3d18f772a8900c9bc1300e7f81c0c05d71f93b770a222f00490ba'
+SHA_ALGORITHM=256
+
+"${INSTALL_LOCAL_PACKAGE:?}" gcloud \
+  "${VERSION:?}" "${URL:?}" google-cloud-sdk/bin/gcloud "${SHA:?}" "${SHA_ALGORITHM:?}"
