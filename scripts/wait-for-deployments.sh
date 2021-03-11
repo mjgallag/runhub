9d1dde2e1abcc73bf6b323266a92e34e8d10c88a
Michael Gallagher <mjgallag@gmail.com>
2021-03-11 08:22:06 -0800

    fix: "quote command substitutions"

--- scripts/wait-for-deployments.sh
+++ scripts/wait-for-deployments.sh
@@ -3,7 +3,7 @@ set -e
 
 NAMESPACE="${1:?}"
 KUBECTL="$("${LOCAL_PACKAGE_INSTALLERS_DIR:?}/kubectl.sh")"
-DEPLOYMENTS=$("${KUBECTL:?}" get --namespace "${NAMESPACE:?}" deployments --output name)
+DEPLOYMENTS="$("${KUBECTL:?}" get --namespace "${NAMESPACE:?}" deployments --output name)"
 
 for DEPLOYMENT in ${DEPLOYMENTS:?}; do
   "${KUBECTL:?}" rollout status --namespace "${NAMESPACE:?}" "${DEPLOYMENT:?}"
