12f9dc9fa706f31a6017261ca6e1210344d197c0
Michael Gallagher <mjgallag@gmail.com>
2021-03-11 09:33:15 -0800

    build: upgrade Helm to v3.5.3 from v3.5.2

--- scripts/local-package-installers/helm.sh
+++ scripts/local-package-installers/helm.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='3.5.2'
+VERSION='3.5.3'
 URL="https://get.helm.sh/helm-v${VERSION:?}-darwin-amd64.tar.gz"
-SHA='68040e9a2f147a92c2f66ce009069826df11f9d1e1c6b78c7457066080ad3229'
+SHA='451ad70dfe286e3979c78ecf7074f4749d93644da8aa2cc778e2f969771f1794'
 SHA_ALGORITHM=256
 
 "${INSTALL_LOCAL_PACKAGE:?}" helm \
