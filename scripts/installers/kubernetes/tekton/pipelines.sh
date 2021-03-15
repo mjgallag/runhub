610125ca1e98ab3d20a1b1d750204a2fa9b0fa8c
Michael Gallagher <mjgallag@gmail.com>
2021-03-15 10:30:28 -0700

    style: remove extra newline

--- scripts/installers/kubernetes/tekton/pipelines.sh
+++ scripts/installers/kubernetes/tekton/pipelines.sh
@@ -4,7 +4,6 @@ set -e
 VERSION='0.22.0'
 KUBECTL="$("${BINARY_INSTALLERS_DIR:?}/kubectl.sh")"
 
-
 curl -L \
   "https://storage.googleapis.com/tekton-releases/pipeline/previous/v${VERSION:?}/release.yaml" \
   | sed 's/disable-affinity-assistant: "false"/disable-affinity-assistant: "true"/' \
