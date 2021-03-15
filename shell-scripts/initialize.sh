46fbcda9ac4626c95b4613e13e5a619b6b993f46
Michael Gallagher <mjgallag@gmail.com>
2021-03-15 14:50:30 -0700

    refactor: pull bin install dependencies out of initialize

--- shell-scripts/initialize.sh
+++ shell-scripts/initialize.sh
@@ -2,15 +2,12 @@
 set -e
 
 BASE_DIR="$(cd "$(dirname "${0:?}")" && pwd)"
-CONFIG_DIR="${BASE_DIR:?}/.runhub"
+export CONFIG_DIR="${BASE_DIR:?}/.runhub"
 SCRIPT_HOME_DIR="${CONFIG_DIR:?}/home"
-BINARIES_DIR="${CONFIG_DIR:?}/binaries"
 
 export BASE_DIR
 export USER_HOME_DIR="${HOME:?}"
 export HOME="${SCRIPT_HOME_DIR:?}"
-export BINARIES_BIN_DIR="${BINARIES_DIR:?}/bin"
-export BINARIES_TMP_DIR="${BINARIES_DIR:?}/tmp"
 export SCRIPTS_DIR="${BASE_DIR:?}/shell-scripts"
 export INSTALLERS_DIR="${SCRIPTS_DIR:?}/installers"
 export BINARY_INSTALLERS_DIR="${INSTALLERS_DIR:?}/bin"
