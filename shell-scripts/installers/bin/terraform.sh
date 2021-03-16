dc0b5c057082b203019e7f5fa2edb1aa605fe4e4
Michael Gallagher <mjgallag@gmail.com>
2021-03-15 18:54:12 -0700

    fix: enable Terraform shared plugin cache

--- shell-scripts/installers/bin/terraform.sh
+++ shell-scripts/installers/bin/terraform.sh
@@ -5,8 +5,11 @@ VERSION='0.14.8'
 URL="https://releases.hashicorp.com/terraform/${VERSION:?}/terraform_${VERSION:?}_darwin_amd64.zip"
 SHA='30115a2ee5f61178527089d8e5da20053927b364b08dc7aee6894a162ccbd793'
 SHA_ALGORITHM=256
-export TF_PLUGIN_CACHE_DIR="${HOME:?}/.terraform.d/plugin-cache"
+PLUGIN_CACHE_DIR="${HOME:?}/.terraform.d/plugin-cache"
 
-mkdir -p "${TF_PLUGIN_CACHE_DIR:?}"
+cat <<EOF > "${HOME:?}/.terraformrc"
+plugin_cache_dir = "${PLUGIN_CACHE_DIR:?}"
+EOF
+mkdir -p "${PLUGIN_CACHE_DIR:?}"
 "${INSTALLERS_DIR:?}/bin/helpers/install.sh" terraform \
   "${VERSION:?}" "${URL:?}" terraform "${SHA:?}" "${SHA_ALGORITHM:?}"
