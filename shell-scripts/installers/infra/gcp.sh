b6065d77fb3796eb85c701f2d13ae8f20780465a
Michael Gallagher <mjgallag@gmail.com>
2021-03-17 10:40:20 -0700

    feat(shell-scripts): allow access to other Google Cloud SDK binaries

--- shell-scripts/installers/infra/gcp.sh
+++ shell-scripts/installers/infra/gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-GCLOUD="$("${INSTALLERS_DIR:?}/bin/gcloud.sh")"
+GCLOUD="$("${INSTALLERS_DIR:?}/bin/google-cloud-sdk.sh")/gcloud"
 
 if ! "${GCLOUD:?}" auth application-default print-access-token \
   --no-user-output-enabled --verbosity none; then
