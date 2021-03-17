0d0134dd9ace2bb2c1e417c324ac84fa9aca22fd
Michael Gallagher <mjgallag@gmail.com>
2021-03-17 08:05:36 -0700

    build: upgrade gcloud to v332.0.0 from v331.0.0

--- shell-scripts/installers/bin/gcloud.sh
+++ shell-scripts/installers/bin/gcloud.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='331.0.0'
+VERSION='332.0.0'
 URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${VERSION:?}-darwin-x86_64.tar.gz"
-SHA='32c7b961f4c3d18f772a8900c9bc1300e7f81c0c05d71f93b770a222f00490ba'
+SHA='1d158a84dfff8f3aa3cacdbbd3fb9cfbea3178728367b20c986760edc1665026'
 SHA_ALGORITHM=256
 
 "${INSTALLERS_DIR:?}/bin/helpers/install.sh" gcloud \
