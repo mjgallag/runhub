b427a59d497e4352cec4fcd10ad3f2bb40fc73ad
Michael Gallagher <mjgallag@gmail.com>
2021-03-19 16:31:01 -0700

    refactor(shell-scripts): have terraform validate env value

--- shell-scripts/installers/infra/helpers/generate-main-tf.sh
+++ shell-scripts/installers/infra/helpers/generate-main-tf.sh
@@ -2,13 +2,22 @@
 set -e
 
 cat <<EOF > "${APP_ENV_TERRAFORM_DIR:?}/main.tf"
+variable "env" {
+  type = string
+  validation {
+    condition     = var.env == "prod" || var.env == "dev"
+    error_message = "The env value must be either 'prod' or 'dev'."
+  }
+  default = "${ENV:?}"
+}
+
 ${VARIABLE_DEFINITIONS:?}
 
 module app_env {
   source = "${SCRIPT_DIR:?}/terraform-modules/${INFRA:?}"
-  
+
   app = "${APP:?}"
-  env = "${ENV:?}"
+  env = var.env
 
 ${VARIABLE_ASSIGNMENTS:?}
 }
