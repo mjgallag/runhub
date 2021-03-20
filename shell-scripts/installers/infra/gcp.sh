28a86c4340bdaa0ebc52c01f7f109486c53f2193
Michael Gallagher <mjgallag@gmail.com>
2021-03-19 17:20:03 -0700

    Revert "Revert "Revert "fix(shell-scripts): gsutil doesn't work with application-default"""
    This reverts commit 75e39096c7d15c5f40d455a762f1ec0b048029f6.

--- shell-scripts/installers/infra/gcp.sh
+++ shell-scripts/installers/infra/gcp.sh
@@ -34,7 +34,7 @@ TERRAFORM="${INSTALLERS_DIR:?}/bin/terraform.sh"
 
 if ! "${GCLOUD:?}" auth application-default print-access-token \
   --no-user-output-enabled --verbosity none; then
-    "${GCLOUD:?}" auth login --update-adc
+    "${GCLOUD:?}" auth application-default login --disable-quota-project --verbosity error
 fi
 
 "${INSTALLERS_DIR:?}/infra/helpers/terraform-apply.sh"
