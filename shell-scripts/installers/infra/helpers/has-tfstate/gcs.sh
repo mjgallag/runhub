63aa725229ebe2be451677d3b3f7d36404072055
Michael Gallagher <mjgallag@gmail.com>
2021-03-19 17:14:46 -0700

    refactor(shell-script): no need for infra specific has tf state check

--- shell-scripts/installers/infra/helpers/has-tfstate/gcs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-GSUTIL="$("${INSTALLERS_DIR:?}/bin/google-cloud-sdk.sh")/gsutil"
-
-"${GSUTIL:?}" stat "gs://${ENV:?}-${APP:?}-terraform-state/default.tfstate"
--- shell-scripts/installers/infra/helpers/terraform-apply.sh
+++ shell-scripts/installers/infra/helpers/terraform-apply.sh
@@ -4,12 +4,13 @@ set -e
 TERRAFORM="${INSTALLERS_DIR:?}/bin/terraform.sh"
 
 "${INSTALLERS_DIR:?}/infra/helpers/generate-main-tf.sh"
+"${INSTALLERS_DIR:?}/infra/helpers/generate-backend-tf.sh"
 
-if "${INSTALLERS_DIR:?}/infra/helpers/has-tfstate/${BACKEND_TYPE:?}.sh"; then
-  "${INSTALLERS_DIR:?}/infra/helpers/generate-backend-tf.sh"
-  "${TERRAFORM:?}" -chdir="${APP_ENV_TERRAFORM_DIR:?}" init -upgrade
+if "${TERRAFORM:?}" -chdir="${APP_ENV_TERRAFORM_DIR:?}" init -upgrade; then
   "${TERRAFORM:?}" -chdir="${APP_ENV_TERRAFORM_DIR:?}" apply -auto-approve
 else
+  rm "${APP_ENV_TERRAFORM_DIR:?}/backend.tf"
+  rm -r "${APP_ENV_TERRAFORM_DIR:?}/.terraform"
   "${TERRAFORM:?}" -chdir="${APP_ENV_TERRAFORM_DIR:?}" init -upgrade
   "${TERRAFORM:?}" -chdir="${APP_ENV_TERRAFORM_DIR:?}" apply -auto-approve
   "${INSTALLERS_DIR:?}/infra/helpers/generate-backend-tf.sh"
