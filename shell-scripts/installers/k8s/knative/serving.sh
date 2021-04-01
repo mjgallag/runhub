ebeb17f03b221c5bc210dbf3676ac8f97d8317f1
Michael Gallagher <mjgallag@gmail.com>
2021-04-01 09:34:38 -0700

    style(shell): reorder args & reduce newlines

--- shell-scripts/installers/k8s/knative/serving.sh
+++ shell-scripts/installers/k8s/knative/serving.sh
@@ -7,8 +7,6 @@ VERSION='0.21.0'
   "https://github.com/knative/serving/releases/download/v${VERSION:?}/serving-crds.yaml"
 "${BIN_DIR:?}/kubectl.sh" apply --filename \
   "https://github.com/knative/serving/releases/download/v${VERSION:?}/serving-core.yaml"
-"${BIN_DIR:?}/kubectl.sh" patch configmap/config-autoscaler \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"allow-zero-initial-scale":"true"}}'
+"${BIN_DIR:?}/kubectl.sh" patch configmap --namespace knative-serving config-autoscaler \
+  --type merge --patch '{"data":{"allow-zero-initial-scale":"true"}}'
 "${INSTALLERS_DIR:?}/k8s/helpers/wait-for-deployments.sh" knative-serving
