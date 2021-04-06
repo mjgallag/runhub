94afce6859f23767117b3d58a05bf9fafa42cddc
Michael Gallagher <mjgallag@gmail.com>
2021-04-06 09:28:05 -0700

    build: upgrade Tekton Pipelines to v0.23.0 from v0.22.0

--- shell-scripts/installers/k8s/tekton/pipelines.sh
+++ shell-scripts/installers/k8s/tekton/pipelines.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-VERSION='0.22.0'
+VERSION='0.23.0'
 
 curl -L \
   "https://storage.googleapis.com/tekton-releases/pipeline/previous/v${VERSION:?}/release.yaml" \
   | sed 's/disable-affinity-assistant: "false"/disable-affinity-assistant: "true"/' \
-  | "${BIN_DIR:?}/kubectl.sh" apply --selector knative.dev/crd-install!=true --filename -
+  | "${BIN_DIR:?}/kubectl.sh" apply --filename -
 "${INSTALLERS_DIR:?}/k8s/helpers/wait-for-deployments.sh" tekton-pipelines
