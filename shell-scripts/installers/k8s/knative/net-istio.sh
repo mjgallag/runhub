5c113f7049dbf48391a465ffd1e6a7823e6bdfb6
Michael Gallagher <mjgallag@gmail.com>
2021-04-08 13:03:32 -0700

    build: Update Knative Net Istio to v0.22.1 from v0.22.0

--- shell-scripts/installers/k8s/knative/net-istio.sh
+++ shell-scripts/installers/k8s/knative/net-istio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-VERSION='0.22.0'
+VERSION='0.22.1'
 
 "${BIN_DIR:?}/kubectl.sh" apply --filename \
   "https://github.com/knative-sandbox/net-istio/releases/download/v${VERSION:?}/net-istio.yaml"
