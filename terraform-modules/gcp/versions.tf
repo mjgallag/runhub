358db84573012ecfacf7b8fd1b243eebcab23363
Michael Gallagher <mjgallag@gmail.com>
2021-04-12 15:05:04 -0700

    build: upgrade Terraform GCP provider to v3.64.0 from v3.63.0

--- terraform-modules/gcp/versions.tf
+++ terraform-modules/gcp/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.63.0"
+      version = "3.64.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "3.63.0"
+      version = "3.64.0"
     }
   }
 }
