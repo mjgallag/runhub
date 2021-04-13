8ae800be67a78438fed44de55199180ac3a6306a
Michael Gallagher <mjgallag@gmail.com>
2021-04-13 16:08:53 -0700

    build: upgrade Google Cloud SDK to v336.0.0 from v335.0.0

--- shell-scripts/installers/bin/gcloud.sh
+++ shell-scripts/installers/bin/gcloud.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='335.0.0'
+VERSION='336.0.0'
 URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${VERSION:?}-darwin-x86_64.tar.gz"
-SHA='10c281ec93214058d04badec1832d57932c11a79e9cf5fa0da4bb0e7f92b1a6d'
+SHA='e1f049c536491e77ff9bfb8e29755ba7006a43c7dd8911350848b668025f6039'
 SHA_ALGORITHM=256
 GCLOUD="$("${INSTALLERS_DIR:?}/bin/helpers/install.sh" gcloud \
   "${VERSION:?}" "${URL:?}" google-cloud-sdk/bin/gcloud "${SHA:?}" "${SHA_ALGORITHM:?}")"
