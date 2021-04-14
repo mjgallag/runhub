0493fd11f812e5c8d7f6dbd91ee887b5657d9460
Michael Gallagher <mjgallag@gmail.com>
2021-04-14 16:32:57 -0700

    build: upgrade Helm to v3.5.4 from v3.5.3

--- shell-scripts/installers/bin/helm.sh
+++ shell-scripts/installers/bin/helm.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='3.5.3'
+VERSION='3.5.4'
 URL="https://get.helm.sh/helm-v${VERSION:?}-darwin-amd64.tar.gz"
-SHA='451ad70dfe286e3979c78ecf7074f4749d93644da8aa2cc778e2f969771f1794'
+SHA='072c40c743d30efdb8231ca03bab55caee7935e52175e42271a0c3bc37ec0b7b'
 SHA_ALGORITHM=256
 HELM="$("${INSTALLERS_DIR:?}/bin/helpers/install.sh" helm \
   "${VERSION:?}" "${URL:?}" darwin-amd64/helm "${SHA:?}" "${SHA_ALGORITHM:?}")"
