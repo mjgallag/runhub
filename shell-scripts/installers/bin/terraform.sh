66d47fc301a9556deda1991fe436a3a9c6f6bef7
Michael Gallagher <mjgallag@gmail.com>
2021-05-06 14:35:13 -0700

    build: upgrade Terraform to v0.15.3 from v0.15.1

--- shell-scripts/installers/bin/terraform.sh
+++ shell-scripts/installers/bin/terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='0.15.1'
+VERSION='0.15.3'
 URL="https://releases.hashicorp.com/terraform/${VERSION:?}/terraform_${VERSION:?}_darwin_amd64.zip"
-SHA='dd7220e6a76e4c9555576c0500bea94c6a5cb65f286b3e74e8ea7cc34bbce5be'
+SHA='2cfa2f896aaf2c2b2fdadea6881f2796fe0d85ad0a42f471aadfb113bc32d11b'
 SHA_ALGORITHM=256
 TERRAFORM="$("${INSTALLERS_DIR:?}/bin/helpers/install.sh" terraform \
   "${VERSION:?}" "${URL:?}" terraform "${SHA:?}" "${SHA_ALGORITHM:?}")"
