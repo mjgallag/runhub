6783214429ffbfd50c7f2f7e14c7f4548e3b6997
Michael Gallagher <mjgallag@gmail.com>
2021-05-12 10:02:00 -0700

    build: upgrade Istio to v1.8.6 from v1.8.5

--- shell-scripts/installers/bin/istioctl.sh
+++ shell-scripts/installers/bin/istioctl.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='1.8.5'
+VERSION='1.8.6'
 URL="https://github.com/istio/istio/releases/download/${VERSION:?}/istioctl-${VERSION:?}-osx.tar.gz"
-SHA='24ac10f0b40d1bb8f01531ea16a25f2c2c29140e95d64d2123b5cba3b99a7e7e'
+SHA='eb89f18fe925d3af2e9a1e9e91269cfbe6bda5d8b1bcffd21f82dc1d244f56cb'
 SHA_ALGORITHM=256
 ISTIOCTL="$("${INSTALLERS_DIR:?}/bin/helpers/install.sh" istioctl \
   "${VERSION:?}" "${URL:?}" istioctl "${SHA:?}" "${SHA_ALGORITHM:?}")"
