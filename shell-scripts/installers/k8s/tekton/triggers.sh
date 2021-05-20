1bcbb5826c7adc3341e15809d92a6097411a86ba
Michael Gallagher <mjgallag@gmail.com>
2021-05-20 09:36:13 -0700

    fix(shell): wait for release deployments before applying interceptors

--- shell-scripts/installers/k8s/tekton/triggers.sh
+++ shell-scripts/installers/k8s/tekton/triggers.sh
@@ -5,6 +5,7 @@ VERSION='0.14.0'
 
 "${BIN_DIR:?}/kubectl.sh" apply --filename \
   "https://storage.googleapis.com/tekton-releases/triggers/previous/v${VERSION:?}/release.yaml"
+"${INSTALLERS_DIR:?}/k8s/helpers/wait-for-deployments.sh" tekton-pipelines
 "${BIN_DIR:?}/kubectl.sh" apply --filename \
   "https://storage.googleapis.com/tekton-releases/triggers/previous/v${VERSION:?}/interceptors.yaml"
 "${INSTALLERS_DIR:?}/k8s/helpers/wait-for-deployments.sh" tekton-pipelines
