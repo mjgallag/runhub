fca3a18ba60380434458b3fadd16903b70c3f436
Michael Gallagher <mjgallag@gmail.com>
2021-05-19 17:18:29 -0700

    build: upgrade Istio to v1.8.6 from v1.8.5

--- shell-scripts/installers/bin/istioctl.sh
+++ shell-scripts/installers/bin/istioctl.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='1.8.6'
+VERSION='1.9.5'
 URL="https://github.com/istio/istio/releases/download/${VERSION:?}/istioctl-${VERSION:?}-osx.tar.gz"
-SHA='eb89f18fe925d3af2e9a1e9e91269cfbe6bda5d8b1bcffd21f82dc1d244f56cb'
+SHA='8b830b5c0854bdee727f44bf36d7505a834eeff4d6047dbd662c86d02acabf05'
 SHA_ALGORITHM=256
 ISTIOCTL="$("${INSTALLERS_DIR:?}/bin/helpers/install.sh" istioctl \
   "${VERSION:?}" "${URL:?}" istioctl "${SHA:?}" "${SHA_ALGORITHM:?}")"
--- shell-scripts/installers/k8s/istio.sh
+++ shell-scripts/installers/k8s/istio.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
 set -e
 
-"${BIN_DIR:?}/istioctl.sh" install --skip-confirmation --filename - <<EOF
-apiVersion: install.istio.io/v1alpha1
-kind: IstioOperator
-spec:
-  values:
-    global:
-      proxy:
-        autoInject: disabled
-      useMCP: false
-      # The third-party-jwt is not enabled on all k8s.
-      # See: https://istio.io/docs/ops/best-practices/security/#configure-third-party-service-account-tokens
-      jwtPolicy: first-party-jwt
-
-  addonComponents:
-    pilot:
-      enabled: true
-
-  components:
-    ingressGateways:
-      - name: istio-ingressgateway
-        enabled: true
-EOF
+"${BIN_DIR:?}/istioctl.sh" install --skip-confirmation
