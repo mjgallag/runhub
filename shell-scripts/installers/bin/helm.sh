4da9d7f4b9c3172b063d52e30af0405a678b28cb
Michael Gallagher <mjgallag@gmail.com>
2021-06-28 16:45:08 -0700

    Revert "build: upgrade Helm to v3.6.1 from v3.5.4"
    This reverts commit eec618d2fe8c2ba2bea7c6c832b8df962a6219bf.

--- shell-scripts/installers/bin/helm.sh
+++ shell-scripts/installers/bin/helm.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-VERSION='3.6.1'
+VERSION='3.5.4'
 URL="https://get.helm.sh/helm-v${VERSION:?}-darwin-amd64.tar.gz"
-SHA='f5e49aac89701162871e576ebd32506060e43a470da1fcb4b8e4118dc3512913'
+SHA='072c40c743d30efdb8231ca03bab55caee7935e52175e42271a0c3bc37ec0b7b'
 SHA_ALGORITHM=256
 HELM="$("${INSTALLERS_DIR:?}/bin/helpers/install.sh" helm \
   "${VERSION:?}" "${URL:?}" darwin-amd64/helm "${SHA:?}" "${SHA_ALGORITHM:?}")"
